3e2b0c998e620e20df30f0fe194eb5a091309cac
Cla Tschenett <design@clatschenett.com>
2017-01-22 14:05:09 +0100

    Images added to MiddleMeetView

---
+++ src/MiddleMeet.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by Cla on 22.01.2017.
+ */
+public class MiddleMeet {
+
+    public static void main(String[] args) {
+        System.out.println("blabla");
+    }
+}
--- src/MiddleMeetView.java
+++ src/MiddleMeetView.java
@@ -1,9 +1,22 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.BorderLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-public class MiddleMeedView extends JFrame {
-    public MiddleMeedView() {
+public class MiddleMeetView extends JFrame {
+
+    public MiddleMeetView() {
+
+        String img;
+        final String INTRO = "/img/intro.jpg";
+        final String ERROR = "/img/error.jpg";
+        final String MAP = "/img/map.jpg";
+
+        img = MAP;
+
 
         //Objects
         JPanel center = new JPanel();
@@ -11,7 +24,7 @@ public class MiddleMeedView extends JFrame {
         JButton B1 = new JButton("OK");
 
         JLabel L1 = new JLabel("Ort 1");
-        JTextField ort1 = new JTextField("",20);
+        JTextField ort1 = new JTextField("",15);
         ort1.setToolTipText("Ort");
         JTextField land1 = new JTextField();
         land1.setToolTipText("Land");
@@ -22,12 +35,25 @@ public class MiddleMeedView extends JFrame {
         JTextField land2 = new JTextField();
         land2.setToolTipText("Land");
 
-        JLabel L3 = new JLabel("CENTER");
+        JLabel LResult = new JLabel("Ergebnis");
+
+
+        JButton calculate = new JButton("Berechne MiddleMeet");
+
+        //Set Image Background
+
+
+        ImageIcon background = new ImageIcon(getClass().getResource(img));
+        JLabel L3 = new JLabel(background);
+
+
         BorderLayout B = new BorderLayout();
-        GridLayout G = new GridLayout(10,0);
+        GridLayout G = new GridLayout(15,0);
         JFrame F = new JFrame();
 
+
         //Properties
+        LResult.setFont(new Font ("Default", Font.BOLD, 20));
 
         F.setLayout(B);
         west.setLayout(G);
@@ -37,18 +63,24 @@ public class MiddleMeedView extends JFrame {
         west.add(L2);
         west.add(ort2);
         west.add(land2);
+        west.add(Box.createVerticalStrut(20));
+        west.add(new JSeparator(SwingConstants.HORIZONTAL));
+        west.add(LResult);
+        west.add(new JSeparator(SwingConstants.HORIZONTAL));
+        west.add(calculate);
+        center.add(L3);
 
         //Margins
         west.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
         add(west, B.WEST);
         add(center, B.CENTER);
 
-
     }
 
     public static void main(String[] args) {
-        MiddleMeedView m = new MiddleMeedView();
-        m.setSize(800, 600);
+        MiddleMeetView m = new MiddleMeetView();
+        m.setTitle("MiddleMeet");
+        m.setSize(840, 680);
         m.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         m.setVisible(true);
     }
